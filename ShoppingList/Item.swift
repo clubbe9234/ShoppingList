d4ed771e85fbfeea5836ba2cb28d64d59c3a9e3d
Caroline Lubbe <clubbe9234@jh-mba158-09.local>
2019-02-12 11:24:58 -0600

    added Item class

---
+++ ShoppingList/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ShoppingList
+//
+//  Created by Caroline Lubbe on 2/12/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
